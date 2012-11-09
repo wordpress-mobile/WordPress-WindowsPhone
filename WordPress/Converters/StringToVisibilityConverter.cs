5a9f8e8889a7046ff15b04a75429fec075fb4b9c
Eric Johnson <aerychj@gmail.com>
2012-11-09 20:30:24 +0000

    Refs #123. Fixes the gap between titles and publish dates.  Uses a Converter to collapse the textbox's visibility if the status is published.

---
+++ WordPress/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WordPress.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+
+            if (value != null && string.IsNullOrWhiteSpace(value.ToString()))
+                return Visibility.Collapsed;
+
+            string postStatus = value as string;
+            if (postStatus.Equals("publish"))
+                return Visibility.Collapsed;
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
