9b77f12b57a40e5189c74bb90303af9060fc88e4
Eric Johnson <aerychj@gmail.com>
2012-11-13 16:20:10 +0000

    Fixes #134. If a comment's author is empty shows "Anonymous". Provides a min height for the comment email and url fields in the list items.

---
+++ WordPress/Converters/AnonymousAuthorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace WordPress.Converters
+{
+    public class AnonymousAuthorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ( value == null || (value != null && string.IsNullOrWhiteSpace(value.ToString())))
+                return "Anonymous";
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
